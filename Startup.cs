e1ae1e0c540b4482ba9a8d7eb09922e16b6ce35e
Elder1360 <mohamm60@outlook.com>
2018-11-13 19:21:51 +0330

    init commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IRRCMS.Startup))]
+namespace IRRCMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
